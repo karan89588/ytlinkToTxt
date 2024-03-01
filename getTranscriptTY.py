63ab0867a376e66037846273cbc8a2906230e4ce
karan89588 <81355355+karan89588@users.noreply.github.com>
2024-03-01 11:51:07 +0530

    Update getTranscriptTY.py
    Any Language Handling

--- getTranscriptTY.py
+++ getTranscriptTY.py
@@ -2,12 +2,29 @@ from youtube_transcript_api import YouTubeTranscriptApi as yta
 
 
 def getResp(video_id):
-    data = yta.get_transcript(video_id)
-    transcript = ""
-    for value in data:
-        for key, val in value.items():
-            if key == "text":
-                transcript += val
-        l = transcript.splitlines()
-        source = " ".join(l)
-    return source
+    l=''
+    try:
+      l=YouTubeTranscriptApi.list_transcripts(video_id)
+      #print('yes')
+      list_lang=[transcript.language_code for transcript in l]
+      lang='en'
+      target_lang='en'
+      text=''
+      if lang not in list_lang:
+        target_lang=list_lang[0]
+        transcript=l.find_transcript([list_lang[0]])
+        translated_transcript=transcript.translate(lang)
+        text_list=translated_transcript.fetch()
+        text_list_1=[t['text'] for t in text_list]
+        text=' '.join(text_list_1)
+      else:
+        transcript=l.find_transcript([lang])
+        text_list=transcript.fetch()
+        text_list_1=[t['text'] for t in text_list]
+        text=' '.join(text_list_1)
+      #print('target_lang ',target_lang)
+      #print('text ',text)
+    except:
+      print('OOps!!! Either link is incorrect or transcript is disable.')
+      return ('na','na')
+    return (text,target_lang)
